1865932bbcd01b85e418fed9294bb7eb8eeea8f9
Mateo Maxm <max.mateo.02@gmail.com>
2023-07-14 21:27:49 -0300

    advances

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const whatsapp = require("wa-multi-session");
+const getNumberFromJid = require('./src/helper/getNumberFromJid');
+const normalizeString = require('./src/helper/normalizeString');
+const DICTIONARY = require('./src/constants/dictionary');
+const getMessageCaption = require('./src/helper/getMessageCaption');
 
 const app = express();
 
 app.set('port', 4000);
-// app.use(cors());
 
-//Middlewares
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded());
-app.use('/api', require('./api'));
+app.use('/api', require('./src/routes/api'));
 
 app.listen(app.get('port'), () => {
     console.log('Servidor Funcionando');
@@ -29,40 +31,16 @@ whatsapp.onConnecting((session) => {
 });
 
 whatsapp.onMessageReceived(async (msg) => {
+    const phoneNumber = getNumberFromJid(msg.key?.remoteJid)
+    if (msg.message?.senderKeyDistributionMessage?.groupId || msg.key?.participant || phoneNumber === '5493816814643') return
     console.log(`New Message =>`, msg);
-    const getNumber = (jid) => {
-        const number = jid.split('@')[0]
-        return number || ''
-    }
-    function normalizeString(texto) {
-        return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    }
-    if (msg.message?.senderKeyDistributionMessage?.groupId || msg.key?.participant) return
-    const textraw = msg.message?.extendedTextMessage?.text?.toLowerCase() ||msg.message?.conversation?.toLowerCase()
+    const textraw = getMessageCaption(msg).toLowerCase()
     const text = normalizeString(textraw)
-    const insults = {
-        gato: 'gata',
-        puto: 'puta',
-        gay: 'gei',
-        gei: 'gei',
-        gai: 'gei',
-        marica: 'marica',
-        pete: 'me hace uno de esos',
-        trolo: 'trola',
-        tragaleche: 'me traga la leche',
-        boludo: 'puto',
-        pelotudo: 'pelotudo',
-        '!gracias': 'De nada tenga buen dia :D',
-        idiota: 'manga de falto de cromosomas',
-        imbecil: 'manga de falto de cromosomas',
-        estupido: 'manga de falto de cromosomas',
-        ridiculo: 'manga de falto de cromosomas',
-    }
-    if (insults[text]) {
+    if (DICTIONARY[text]) {
         await whatsapp.sendTextMessage({
             sessionId: "qeso", 
-            to: getNumber(msg.key?.remoteJid),
-            text: `tu vieja ${insults[text]}`,
+            to: phoneNumber,
+            text: `${DICTIONARY[text]}`,
         });
     }
 });
